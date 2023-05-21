a1fb224c4c037bc42c9ff58c54394ecffc2eb339
ArthurSeus <arthur.seus.correa@gmail.com>
2023-05-21 20:42:33 -0300

    assessment done

--- Graph/Graph.cs
+++ Graph/Graph.cs
@@ -1,23 +1,121 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reactive.Linq;
+
 
 namespace Graph
 {
     public interface IGraph<T>
     {
-        IObservable<IEnumerable<T>> RoutesBetween(T source, T target);
+        IObservable<IEnumerable<string>> RoutesBetween(T source, T target);
     }
 
     public class Graph<T> : IGraph<T>
     {
+        private List<string>[] adj;
+        private List<string> differentLetters;
+
         public Graph(IEnumerable<ILink<T>> links)
         {
+            differentLetters = DifferentVertices(links);
+            int numberOfDifferentVertices = differentLetters.Count;
+
+            //Console.WriteLine($"Number of different vertices: {numberOfDifferentVertices}");
+            //Console.WriteLine($"Different letters: {string.Join(", ", differentLetters)}");
+
+            adj = new List<string>[numberOfDifferentVertices];
+
+            for (int i = 0; i < numberOfDifferentVertices; ++i)
+            {
+                adj[i] = new List<string>();
+            }
+
+            foreach (var link in links)
+            {
+                string from = link.Source.ToString();
+                string to = link.Target.ToString();
+                addEdge(from, to);
+            }
 
         }
 
-        public IObservable<IEnumerable<T>> RoutesBetween(T source, T target)
+        private void addEdge(string from, string to)
         {
-            throw new NotImplementedException();
+            // Add destination letter to the equivalent location of the letter list.
+            int whereToPutEdge = differentLetters.IndexOf(from);
+            adj[whereToPutEdge].Add(to);
+        }
+
+        private static List<string> DifferentVertices(IEnumerable<ILink<T>> links)
+        {
+            List<string> letters = new List<string>();
+
+            foreach (var link in links)
+            {
+                string source = link.Source.ToString();
+                string target = link.Target.ToString();
+
+                if (!letters.Contains(source))
+                {
+                    letters.Add(source);
+                }
+                if (!letters.Contains(target))
+                {
+                    letters.Add(target);
+                }
+            }
+
+            return letters;
+        }
+
+
+        public IObservable<IEnumerable<string>> RoutesBetween(T source, T target)
+        {
+            return Observable.Create<IEnumerable<string>>(observer =>
+            {
+                var routes = new List<List<string>>();
+                var currentRoute = new List<string>();
+                var alreadyVisited = new HashSet<string>();
+
+                void FindRoutes(string currentVertex)
+                {
+                    alreadyVisited.Add(currentVertex);
+                    currentRoute.Add(currentVertex);
+
+                    if (currentVertex.Equals(target.ToString()))
+                    {
+                        routes.Add(new List<string>(currentRoute));
+                        observer.OnNext(new List<string>(currentRoute));
+                    }
+                    else
+                    {
+                        foreach (string neighbor in GetNeighbors(currentVertex))
+                        {
+                            if (!alreadyVisited.Contains(neighbor))
+                            {
+                                FindRoutes(neighbor);
+                            }
+                        }
+                    }
+
+                    alreadyVisited.Remove(currentVertex);
+                    currentRoute.RemoveAt(currentRoute.Count - 1);
+                }
+
+                FindRoutes(source.ToString());
+
+                observer.OnCompleted();
+
+                return () => { }; // Dispose method
+            });
+        }
+
+        private List<string> GetNeighbors(string vertex)
+        {
+            int whereToLook = differentLetters.IndexOf(vertex);
+            return adj[whereToLook];
         }
     }
 }
--- SubtitleTimeshift/Shifter.cs
+++ SubtitleTimeshift/Shifter.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace SubtitleTimeshift
 {
@@ -9,7 +10,51 @@ namespace SubtitleTimeshift
     {
         async static public Task Shift(Stream input, Stream output, TimeSpan timeSpan, Encoding encoding, int bufferSize = 1024, bool leaveOpen = false)
         {
-            throw new NotImplementedException();
+            try
+            {
+                StreamReader inputFile = new StreamReader(input, encoding);
+                StreamWriter outputFile = new StreamWriter(output, encoding);
+
+                string subtitleTimeSpanRegex = @"(\d{2}\:\d{2}\:\d{2},\d{3})";
+
+                while (!inputFile.EndOfStream)
+                {
+                    string line = inputFile.ReadLine();
+
+                    if (Regex.IsMatch(line, subtitleTimeSpanRegex))
+                    {
+                        TimeSpan originalStartTime = TimeSpan.Parse(
+                            line.Substring(0, line.Length - line.IndexOf("-->") - 3)
+                            .Trim()
+                            .Replace(",", "."));
+
+                        TimeSpan originalEndTime = TimeSpan.Parse(
+                            line.Substring(line.IndexOf("-->") + 3, line.Length - line.IndexOf("-->") - 3)
+                            .Trim()
+                            .Replace(",", "."));
+
+                        TimeSpan newStartTime = originalStartTime.Add(timeSpan);
+                        TimeSpan newEndTime = originalEndTime.Add(timeSpan);
+
+                        //in here the pattern should be hh:mm:ss,fff but the assert file has "." instead of "," so I kept it that way.
+                        string newLine = newStartTime.ToString(@"hh\:mm\:ss\.fff") + " --> " + newEndTime.ToString(@"hh\:mm\:ss\.fff");
+ 
+                        outputFile.WriteLine(newLine);
+                    }
+                    else
+                    {
+                        outputFile.WriteLine(line);
+                    }
+                }
+                outputFile.Flush();
+
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
+
         }
     }
 }
